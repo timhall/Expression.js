9834f11944fa588ead3b5cf1a40294e18c0cf296
Tim Hall <tim.hall.engr@gmail.com>
2013-04-30 11:45:27 -0400

    Add build and test automation

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+        meta: {
+            banner:
+                '// Expression.js - Advanced expression evaluation in javascript\n' +
+                '// (c) Tim Hall - https://github.com/timhall/Expression.js - License: MIT\n' +
+                '\n'
+        },
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            src: ['Expression.js']
+        },
+
+        preprocess: {
+            build: {
+                files: {
+                    'Expression.js': 'src/build/Expression.js'
+                }
+            }
+        },
+        
+        concat: {
+            options: {
+                banner: '<%= meta.banner %>'
+            },
+            build: {
+                src: ['Expression.js'],
+                dest: 'Expression.js'
+            }
+        },
+        
+        simplemocha: {
+            options: {
+                globals: [],
+                timeout: 3000,
+                ignoreLeaks: false,
+                ui: 'bdd',
+                reporter: 'spec'
+            },
+
+            all: { 
+                src: ['test/**/*.js']
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['src/**/*.js', 'test/**/*.js'],
+                tasks: ['test']
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-preprocess');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-simple-mocha');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('test', ['preprocess:build', 'concat:build', 'jshint', 'simplemocha']);
+    grunt.registerTask('default', ['test']);
+
+};
