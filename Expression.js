5ad42de4733fc4b89f029417641033f54dc227ef
Tim Hall <tim.hall.engr@gmail.com>
2013-04-30 22:02:05 -0400

    Add exponent operator

--- Expression.js
+++ Expression.js
@@ -189,13 +189,15 @@ Expression.operators = {
         '+': 3,
         '-': 3,
         '*': 5,
-        '/': 5
+        '/': 5,
+        '^': 6
     },
     associative: {
         '+': 'left',
         '-': 'left',
         '*': 'left',
-        '/': 'left'
+        '/': 'left',
+        '^': 'right'
     },
 
     '+': function add(a, b) {
@@ -209,6 +211,9 @@ Expression.operators = {
     },
     '/': function divide(a, b) {
         return a / b;
+    },
+    '^': function power(a, b) {
+        return a ^ b;
     }
 };
 
--- src/operators.js
+++ src/operators.js
@@ -4,13 +4,15 @@ Expression.operators = {
         '+': 3,
         '-': 3,
         '*': 5,
-        '/': 5
+        '/': 5,
+        '^': 6
     },
     associative: {
         '+': 'left',
         '-': 'left',
         '*': 'left',
-        '/': 'left'
+        '/': 'left',
+        '^': 'right'
     },
 
     '+': function add(a, b) {
@@ -24,5 +26,8 @@ Expression.operators = {
     },
     '/': function divide(a, b) {
         return a / b;
+    },
+    '^': function power(a, b) {
+        return a ^ b;
     }
 };
