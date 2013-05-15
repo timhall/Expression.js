0ee62a4c114294c8e1c25297139fa1b1e9e32c00
Tim Hall <tim.hall.engr@gmail.com>
2013-05-15 09:05:28 -0400

    Spike Function() implementation

--- src/Expression.js
+++ src/Expression.js
@@ -11,22 +11,44 @@ function Expression(expression, options) {
     }
 
     this.expression = expression;
-    this.options = Expression.utils.extend({}, Expression.defaults, options);
+    //this.options = Expression.utils.extend({}, Expression.defaults, options);
 
-    // Convert expression to RPN and prepare for evaluation
-    this._tokens = Expression.convertToTokens(this.expression);
-    this._rpn = expression.convertToRPN(this._tokens);
+    // "Compile" function
+ 	this.compiled = new Function('state', 'fn', 'bi', 'un', this.build() + '; return state;');
+
+ 	return this;
 }
 
-Expression.defaults = {};
-Expression.functions = {};
-
-/**
- * Evaluate expression using given variables
- *
- * @param {Object} variables
- * @return {Varies}
- */
-Expression.prototype.evaluate = function (variables) {
-
+Expression.prototype.build = function () {
+	// this.expression: c = a + b
+	return "state['c'] = bi['+'](state['a'], state['b'])";
 };
+
+Expression.prototype.evaluate = function (state) {
+	return this.compiled(state, Expression.fn, Expression.bi, Expression.un);
+}
+
+Expression.operators = {
+	binary: {
+		'+': { precedence: 3, associative: 'left' },
+		'-': { precedence: 3, associative: 'left' },
+		'*': { precedence: 5, associative: 'left' },
+		'/': { precedence: 5, associative: 'left' },
+		'^': { precedence: 6, associative: 'right' }
+	},
+	unary: {
+		'-': { precedence: 4 }
+	}
+};
+
+Expression.bi = {
+	'+': function (a, b) { return a + b; },
+	'-': function (a, b) { return a - b; },
+	'*': function (a, b) { return a * b; },
+	'/': function (a, b) { return a / b; },
+	'^': function (a, b) { return Math.pow(a, b); }
+};
+Expression.un = {
+	'-': function (a) { return -a; }
+};
+Expression.fn = {};
