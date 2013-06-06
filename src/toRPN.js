032c854d2cfeabd28a50626e3d6872c3c3cc5584
Tim Hall <tim.hall.engr@gmail.com>
2013-06-06 13:16:46 -0700

    Remove RPN, tokens, and other unused code

--- src/toRPN.js
+++
@@ -1,9 +0,0 @@
-/**
- * Convert given expression tokens to RPN stack
- *
- * @param {Array} tokens
- * @return {Array} RPN operations stack
- */
-Expression.toRPN = function (tokens) {
-        
-};
